855b5a041a57329f84b832469438196f13943963
James Stanley <james@incoherency.co.uk>
2023-04-10 11:54:23 +0100

    Support pipelined requests

--- src/main.zig
+++ src/main.zig
@@ -20,6 +20,8 @@ pub fn main() !void {
             std.debug.print("accept: {}\n", .{err});
             std.process.exit(1);
         };
+        // TODO: how expensive is spawning a thread? would we rather use async
+        // io? would we rather have a thread pool with aysnc within each pool?
         _ = std.Thread.spawn(.{}, handler, .{conn}) catch |err| {
             std.debug.print("std.Thread.spawn: {}\n", .{err});
             conn.stream.close();
@@ -47,17 +49,33 @@ fn handleRequest(conn: std.net.StreamServer.Connection) !void {
     var buf: [1000]u8 = undefined;
     while (true) {
         const request = try nextLine(conn.stream.reader(), &buf) orelse break;
+        // TODO: extract important headers; in particular we (might) care about
+        // Host, Connection, Range, ...?
+        try skipHeaders(conn);
         std.debug.print("> {s}\n", .{request});
         const response = response_table.get(request) orelse {
             try send404(conn);
+            // TODO: pipelining for 404s?
             break;
         };
         try conn.stream.writeAll(response);
-        break;
+        // TODO: read request body, if any? do whatever is needed to either read
+        // up to the point where we're expecting a new request, or else cleanly
+        // close the connection
+    }
+}
+
+// keep reading until we pass an empty line
+fn skipHeaders(conn: std.net.StreamServer.Connection) !void {
+    var buf: [1000]u8 = undefined;
+    while (true) {
+        const line = try nextLine(conn.stream.reader(), &buf) orelse return error.EndOfStream;
+        if (line.len == 0) break;
     }
 }
 
 fn send404(conn: std.net.StreamServer.Connection) !void {
+    // TODO: read from /404.html ?
     try conn.stream.writeAll("HTTP/1.1 404 Not Found\r\nContent-type: text/plain\r\nConnection: close\r\n\r\n404\n");
 }
 
@@ -86,7 +104,7 @@ fn addFiles(dir: std.fs.IterableDir, name: []const u8) !void {
 fn addFile(file: std.fs.File, name: []const u8) !void {
     const content = try file.readToEndAlloc(std.heap.page_allocator, max_file_size);
     const headers10 = try std.fmt.allocPrint(std.heap.page_allocator, "Content-type: text/html\r\nContent-length: {d}\r\n", .{content.len});
-    const headers11 = try std.fmt.allocPrint(std.heap.page_allocator, "Connection: close\r\nContent-type: text/html\r\nContent-length: {d}\r\n", .{content.len});
+    const headers11 = try std.fmt.allocPrint(std.heap.page_allocator, "Connection: keep-alive\r\nContent-type: text/html\r\nContent-length: {d}\r\n", .{content.len});
     try response_table.put(try std.fmt.allocPrint(std.heap.page_allocator, "GET {s} HTTP/1.0", .{name}), try std.fmt.allocPrint(std.heap.page_allocator, "HTTP/1.0 200 OK\r\n{s}\r\n{s}", .{ headers10, content }));
     try response_table.put(try std.fmt.allocPrint(std.heap.page_allocator, "GET {s} HTTP/1.1", .{name}), try std.fmt.allocPrint(std.heap.page_allocator, "HTTP/1.1 200 OK\r\n{s}\r\n{s}", .{ headers11, content }));
     try response_table.put(try std.fmt.allocPrint(std.heap.page_allocator, "HEAD {s} HTTP/1.0", .{name}), try std.fmt.allocPrint(std.heap.page_allocator, "HTTP/1.0 200 OK\r\n{s}\r\n", .{headers10}));
